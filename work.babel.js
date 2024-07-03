bcddbb8cb99978516921f6be4b0829d9c8b25a49
shinjiyun <ranji12@naver.com>
2024-07-03 11:55:28 +0900

    하위버전 번들링

---
+++ work.babel.js
@@ -0,0 +1,14 @@
+"use strict";
+
+function add(a, b) {
+  var result = a + b;
+  return result;
+}
+function minam() {
+  var obj = {
+    name: "신지윤",
+    age: 25,
+    job: "미덕"
+  };
+  return obj;
+}
